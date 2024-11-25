faf0c78cca5f1b82c52251745233a4b9ee881c61
PerezRick <perezrick358@gmail.com>
2024-11-25 21:03:51 +0800

    GreaterNum1Num2.java

---
+++ GreaterNum1Num2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		System.out.println("Enter First Number");
+		double firstNum = sc.nextDouble();
+		System.out.println("Enter Second Number");
+		double secondNum = sc.nextDouble();
+		
+		if (firstNum>secondNum){
+		    System.out.println("The First number is greater than the second number");
+		    }else{
+		   		        		        
+		        }
+		if(secondNum>firstNum){
+		    System.out.println("The second number is greater than first number");
+		    }else{
+		        }
+		if (firstNum==secondNum){
+		    System.out.println("Both numbers are equal");
+		    }else{
+		        }
+	}
+}
